e25a858a739c43c3e2537255d60784a5b22a20a2
Chelsi Christmas <c.christmas95@gmail.com>
2019-11-14 15:03:00 -0500

    Basic Interface Skeleton

---
+++ Hangman/Hangman/PlayScreenViewController.swift
@@ -0,0 +1,33 @@
+//
+//  PlayScreenViewController.swift
+//  Hangman
+//
+//  Created by Chelsi Christmas on 11/14/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class PlayScreenViewController: UIViewController {
+    
+    @IBOutlet weak var blankSpaceLabel: UILabel!
+       @IBOutlet weak var textField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
